0040e27f0dacec07e3dbd176478d0291b15505f8
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-06-16 21:09:27 -0700

    Completed Likes Task

--- Js/likes/script.js
+++ Js/likes/script.js
@@ -1,15 +1,20 @@
-var numOfLikesArr = [document.querySelector("#numberOfLikesUser0").innerText,
-  document.querySelector("#numberOfLikesUser1").innerText];
+// var numOfLikesArr = [document.querySelector("#numberOfLikesUser0").innerText,
+//   document.querySelector("#numberOfLikesUser1").innerText,
+//   document.querySelector("#numberOfLikesUser2").innerText]
+
+// function like(element) {
+
+
+//   for (var i = 0; i < numOfLikesArr.length; i++) {
+//     if (element.id == i) {
+//       numOfLikesArr[i]++
+//     }
+//   }
+//   document.querySelector("#numberOfLikesUser0").innerText = numOfLikesArr[0]
+//   document.querySelector("#numberOfLikesUser1").innerText = numOfLikesArr[1]
+//   document.querySelector("#numberOfLikesUser2").innerText = numOfLikesArr[2]
+// }
 
 function like(element) {
-  // document.querySelector("#numberOfLikes").innerText++
-
-  for (var i = 0; i < numOfLikesArr.length; i++) {
-    if (element.id == i) {
-      numOfLikesArr[i]++
-    }
-  }
-  document.querySelector("#numberOfLikesUser0").innerText = numOfLikesArr[0]
-  document.querySelector("#numberOfLikesUser1").innerText = numOfLikesArr[1]
-
+  document.querySelector(element).innerText++
 }
